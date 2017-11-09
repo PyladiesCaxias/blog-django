eff43080b471188b70f175df92180cfbf63e51a4
Eliane Faveron Maciel <elinefav94@gmail.com>
2017-11-09 21:21:14 -0200

    Adicionado urls, views e templates

--- blog/app_blog/views.py
+++ blog/app_blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from app_blog.models import *
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
